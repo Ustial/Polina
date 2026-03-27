d4eb6d4a227433b43e0c97873a07e0dbf4f00ba2
Ustial <118056835+Ustial@users.noreply.github.com>
2026-03-27 14:44:16 +0500

    Delete compliment-site directory

--- compliment-site/data/photos.js
+++
@@ -1,5 +0,0 @@
-window.PHOTOS = [
-  { src: "assets/images/photos/1.png", alt: "Фото 1" },
-  { src: "assets/images/photos/2.png", alt: "Фото 2" },
-  { src: "assets/images/photos/3.png", alt: "Фото 3" },
-];
